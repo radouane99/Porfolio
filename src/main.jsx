4e192fb8429f09da76039eae99f7c80887e41093
Radouane <radouane.asri99@gmail.com>
2026-02-23 02:31:00 +0000

    change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/porfolio">
+    <BrowserRouter basename="/Portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>,
